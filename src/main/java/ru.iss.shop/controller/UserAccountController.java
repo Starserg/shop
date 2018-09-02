c867e9c0dd2428c33785e6fa56835d5b6be7277b
Starostin Sergey <40168244+Starserg@users.noreply.github.com>
2018-09-02 11:01:07 +0400

    userAccount: start

---
+++ src/main/java/ru.iss.shop/controller/UserAccountController.java
@@ -0,0 +1,16 @@
+package ru.iss.shop.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import ru.iss.shop.domain.Product;
+
+@Controller
+public class UserAccountController {
+    @GetMapping("/userAccount")
+    public String getProducts(Model model) {
+
+        return "userAccount";
+    }
+}
